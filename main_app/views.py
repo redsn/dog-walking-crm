b59254e5a996ebd598b3724057ce6c62e4e48af7
tkl <tluk92@gmail.com>
2022-10-01 19:40:14 -0400

    .

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Dog, Activity
+from .forms import ActivityForm
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
@@ -14,11 +15,18 @@ def about(request):
 def dogs_index(request):
     pass
 
-def dogs_detail(request):
-    pass
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    activity_form = ActivityForm()
+    return render(request, 'dogs/detail.html', {'dog': dog, 'activity_form': activity_form})
 
-def add_activity(request):
-    pass
+def add_activity(request, dog_id):
+    form = ActivityForm(request.POST)
+    if form.is_valid():
+        new_activity = form.save(commit=False)
+        new_activity.dog_id = dog_id
+        new_activity.save()
+    return redirect('dog_detail', dog_id=dog_id)
 
 class DogCreate(CreateView):
     model = Dog
