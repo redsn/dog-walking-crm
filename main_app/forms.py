664958fe4245ca5c7be314099b8416f94bc1fefc
Cory Dela Cruz <corydcruz@gmail.com>
2022-10-05 19:59:15 -0700

    navbar has profile url, starting signupform updates

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,20 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 from .models import Activity
+from django.contrib.auth.models import User
+from django import forms
+
 
 class ActivityForm(ModelForm):
     class Meta:
         model = Activity
         fields = ('date', 'activity')
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+
+    class Meta: 
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
